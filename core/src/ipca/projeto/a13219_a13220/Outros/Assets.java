0c92e9b2138d7a49ce0e4b9feeed278027bf9215
Cmdsa <carlos-.-alves@hotmail.com>
2018-01-13 00:14:25 +0000

    Pequenas modificaçoes, adicionar algumas imagens, colocar um hud simples

--- core/src/ipca/projeto/a13219_a13220/Outros/Assets.java
+++ core/src/ipca/projeto/a13219_a13220/Outros/Assets.java
@@ -1,8 +1,37 @@
 package ipca.projeto.a13219_a13220.Outros;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
 /**
  * Created by Carlos Alves on 13/01/2018.
  */
 
 public class Assets {
+    public static Texture background;
+    public static Texture playButton;
+    public static Texture scoreButton;
+    public static Texture scoreButtonclick;
+    public static Texture optionsButton;
+    public static Texture arrow;
+    public static Sound clickSound;
+    public static BitmapFont font;
+
+
+    public static void load () {
+        background = new Texture("imagens/commie.jpg");
+        playButton = new Texture("imagens/FoiceMartelo.png");
+        scoreButton = new Texture("imagens/score.png");
+        optionsButton = new Texture("imagens/defenicoes.png");
+        scoreButtonclick =  new Texture("imagens/score2.png");
+        arrow = new Texture("imagens/arrow.png");
+        clickSound = Gdx.audio.newSound(Gdx.files.internal("Sound/Click.wav"));
+        font =new BitmapFont(Gdx.files.internal("stuff.fnt"),Gdx.files.internal("stuff.png"),false);
+    }
+
+    public static void playSound (Sound sound) {
+        if (Options.soundEnabled) sound.play(1);
+    }
 }
