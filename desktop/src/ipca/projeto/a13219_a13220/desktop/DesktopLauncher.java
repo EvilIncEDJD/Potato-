f98d2a55ce38ef3ac23bb3ab2602d8c51af8584d
KaiserFrost <brunossj5@hotmail.com>
2018-01-14 18:21:53 +0000

    Pequena alteração
    size do ecra

--- desktop/src/ipca/projeto/a13219_a13220/desktop/DesktopLauncher.java
+++ desktop/src/ipca/projeto/a13219_a13220/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import ipca.projeto.a13219_a13220.Potato;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Potato.V_WIDTH ;
+		config.height = Potato.V_HEIGHT;
 		new LwjglApplication(new Potato(), config);
 	}
 }
