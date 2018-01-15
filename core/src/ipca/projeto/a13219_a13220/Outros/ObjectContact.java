11a524145d0195b88af08fd7a4274cc2d4308279
KaiserFrost <brunossj5@hotmail.com>
2018-01-15 00:28:29 +0000

    Varias classes
    classe batata que dá os pontos aos inimigos, classe martelo, que sai do jogador, gameoverscrenn

---
+++ core/src/ipca/projeto/a13219_a13220/Outros/ObjectContact.java
@@ -0,0 +1,8 @@
+package ipca.projeto.a13219_a13220.Outros;
+
+/**
+ * Created by Bruno on 14/01/2018.
+ */
+
+public class ObjectContact {
+}
---
+++ core/src/ipca/projeto/a13219_a13220/Screens/GameOverScreen.java
@@ -0,0 +1,8 @@
+package ipca.projeto.a13219_a13220.Screens;
+
+/**
+ * Created by Bruno on 14/01/2018.
+ */
+
+public class GameOverScreen {
+}
